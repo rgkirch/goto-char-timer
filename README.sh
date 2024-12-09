83ef23dce61a09aee039479be9064426b4bb9ef5
me <me@DuckBookPro.local>
2024-12-09 09:07:59 -0500

    docs: update README.sh to clarify timeout and character set settings

--- README.sh
+++ README.sh
@@ -35,9 +35,9 @@ There are other similar extensions but I didn't find any that worked the way I w
 
 ## Extension Settings
 
-TODO timeout
+timeout
 
-TODO character set to use for jump completions
+character set to use for jump completions
 
 e.g.
 
